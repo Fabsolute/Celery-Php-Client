631708ff6c2b3118b58df3b88ef7756d00567d8d
Ahmet Türk <ahmetturk@Ahmets-iMac.local>
2017-04-14 12:38:21 +0300

    INITIAL Commit

---
+++ src/Fabs/Celery/Celery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmetturk
+ * Date: 14/04/2017
+ * Time: 12:34
+ */
+
+namespace Fabs\Celery;
+
+
+class Celery
+{
+    
+}
---
+++ src/Fabs/Celery/Exception/CeleryTimeoutException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ahmetturk
+ * Date: 14/04/2017
+ * Time: 12:36
+ */
+
+namespace Fabs\Celery\Exception;
+
+
+class CeleryTimeoutException extends CeleryException
+{
+
+}
